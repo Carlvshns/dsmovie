5654509f810ad07a91ae848b716215e44990f0af
Carlos <mugetsuvita@gmail.com>
2022-05-17 18:45:00 -0300

    fix

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/Carlvshns">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/Carlvshns</p>
                         </div>
                     </a>
                 </div>
